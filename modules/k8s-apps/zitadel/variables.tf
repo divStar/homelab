fbfd02722a23db997e9bfebba5f5bee8c4faa704
Dr. Steven Strange <strange@my.family>
2025-07-09 02:41:14 +0200

    Removing old zitadel directory, adding project.auto.tfvars.example for postgres

--- modules/k8s-apps/zitadel/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "versions_yaml" {
-  description = "Path to the `versions.yaml` file, that contains all relevant versions"
-  type        = string
-  default     = "../../versions.yaml"
-  nullable    = false
-}

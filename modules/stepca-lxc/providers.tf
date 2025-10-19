2d1ea893c6649acb836e6508ac7d3d9fb6182438
divStar <divStar@gmail.com>
2025-10-20 01:50:34 +0200

    feat(step-ca): Step CA LXC now works, but it requires you to manually add the proper debian repository

--- modules/stepca-lxc/providers.tf
+++ modules/stepca-lxc/providers.tf
@@ -4,11 +4,15 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = ">= 0.78.1"
+      version = ">= 0.85.1"
     }
     ssh = {
       source  = "loafoe/ssh"
-      version = "~> 2.7"
+      version = ">= 2.7.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.7.2"
     }
   }
 }
